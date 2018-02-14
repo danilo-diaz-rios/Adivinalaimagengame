8f26599093391a35ea86441bd0e2e5cbda6705c2
Danilo enrique  Diaz rios <daniloenriquediazrios@MacBook-Pro-de-Danilo.local>
2018-02-14 14:46:47 -0500

    Initial commit

---
+++ app/src/main/java/com/example/daniloenriquediazrios/adivinalaimagengame/Ayuda.kt
@@ -0,0 +1,12 @@
+package com.example.daniloenriquediazrios.adivinalaimagengame
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class Ayuda : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_ayuda)
+    }
+}
