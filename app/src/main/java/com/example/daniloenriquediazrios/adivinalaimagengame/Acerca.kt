d392d3c104b9aa298655c4bd0efa9978ccbc5ed5
daniloenriquediazrios <danilodiaz2044@gmail.com>
2018-02-26 08:38:47 -0500

    modificacion 3

---
+++ app/src/main/java/com/example/daniloenriquediazrios/adivinalaimagengame/Acerca.kt
@@ -0,0 +1,12 @@
+package com.example.daniloenriquediazrios.adivinalaimagengame
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class Acerca : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_acerca)
+    }
+}
--- app/src/main/java/com/example/daniloenriquediazrios/adivinalaimagengame/MainActivity.kt
+++ app/src/main/java/com/example/daniloenriquediazrios/adivinalaimagengame/MainActivity.kt
@@ -1,12 +1,39 @@
 package com.example.daniloenriquediazrios.adivinalaimagengame
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        button_jugar.setOnClickListener{
+
+            val boton_jugar = Intent(this,jugar::class.java)
+            startActivity(boton_jugar)
+
+
+        }
+
+        button_ayuda.setOnClickListener{
+
+            val boton_ayuda = Intent( this,Ayuda::class.java)
+            startActivity(boton_ayuda)
+        }
+
+
+        button_acerca.setOnClickListener{
+
+            val boton_acerca = Intent( this,Acerca::class.java)
+            startActivity(boton_acerca)
+        }
     }
+
+
+
 }
