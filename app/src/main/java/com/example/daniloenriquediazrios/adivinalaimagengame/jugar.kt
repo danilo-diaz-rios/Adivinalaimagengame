e35b9981316bbf84075fe1ef67c060b2ab89c052
daniloenriquediazrios <danilodiaz2044@gmail.com>
2018-02-26 11:33:51 -0500

    modificacion 5

--- app/src/main/java/com/example/daniloenriquediazrios/adivinalaimagengame/jugar.kt
+++ app/src/main/java/com/example/daniloenriquediazrios/adivinalaimagengame/jugar.kt
@@ -2,11 +2,13 @@ package com.example.daniloenriquediazrios.adivinalaimagengame
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class jugar : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_jugar)
+
     }
 }
