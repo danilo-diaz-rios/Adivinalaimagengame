e8ab98b2d3841e312cdbc0e3033f5e674bd66b22
daniloenriquediazrios <danilodiaz2044@gmail.com>
2018-04-04 12:03:24 -0500

    modificacion 6 finalizando

--- app/src/main/java/com/example/daniloenriquediazrios/adivinalaimagengame/jugar.kt
+++ app/src/main/java/com/example/daniloenriquediazrios/adivinalaimagengame/jugar.kt
@@ -1,5 +1,6 @@
 package com.example.daniloenriquediazrios.adivinalaimagengame
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
@@ -7,11 +8,13 @@ import kotlinx.android.synthetic.main.activity_jugar.*
 
 class jugar : AppCompatActivity() {
 
+
     var listimages= hashMapOf<String,Int>( "Tsunade" to R.mipmap.img12, "Goku" to R.mipmap.img10,
             "Naruto" to R.mipmap.img11, "Vegeta" to R.mipmap.img13, "Black" to  R.mipmap.img2,
             "Charizard" to R.mipmap.img3, "Gengar" to R.mipmap.img4, "Articuno" to R.mipmap.img5,
             "Shikamaru" to R.mipmap.img6, "Hinata" to R.mipmap.img7, "Gohan" to R.mipmap.img8,
             "Azuma" to R.mipmap.img9)
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -29,6 +32,9 @@ class jugar : AppCompatActivity() {
         imageView.setImageResource(listimages.get("Hinata")!!)
         imageView.setImageResource(listimages.get("Gohan")!!)
         imageView.setImageResource(listimages.get("Azuma")!!)
+
+
+
 
 
 
